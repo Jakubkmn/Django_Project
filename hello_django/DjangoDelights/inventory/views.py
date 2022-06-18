5a6871e305fbefec2ddd7eef2a08c207c8fb1d0a
Jakub Komon <01158683@pw.edu.pl>
2022-06-18 20:38:06 +0200

    created views

--- hello_django/DjangoDelights/inventory/views.py
+++ hello_django/DjangoDelights/inventory/views.py
@@ -1,7 +1,31 @@
+from distutils.log import Log
+from typing import List
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
+from .models import Ingredient, MenuItem, Purchase, RecipeRequirement
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'base.html'
+
+
+class IngredientView(LoginRequiredMixin, ListView):
+    model = Ingredient
+    template_name = 'ingredient_list.html'
+
+class IngredientDelete(LoginRequiredMixin, DeleteView):
+    model = Ingredient
+    template_name = 'ingredient_delete.html'
+
+class MenuView(LoginRequiredMixin, ListView):
+    model = MenuItem
+    template_name = 'menu_view.html'
+
+class PurchaseView(LoginRequiredMixin, ListView):
+    model = Purchase
+    template_name = 'purchase_view.html'
+
+class Report(LoginRequiredMixin, TemplateView):
+    template_name = 'report.html'
